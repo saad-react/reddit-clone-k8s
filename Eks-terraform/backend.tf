b330c6660bb27f27087c3cc5cff4b96248cacbae
saad-react <67101229+saad-react@users.noreply.github.com>
2024-07-02 15:54:01 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "reditprac" # Replace with your actual S3 bucket name
+    bucket = "reddit7224" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
